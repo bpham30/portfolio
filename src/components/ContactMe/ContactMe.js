d038c87b2a2cea08739e60978f35bfabdfcbdbe2
Bang Pham <bangpham@Bangs-Air.attlocal.net>
2022-02-05 06:46:38 -0500

    new navbar, landing page layout change, updated for mobile

---
+++ src/components/ContactMe/ContactMe.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function ContactMe() {
+  return (
+    <div className="colz">
+              <div className='colz-icon'>
+            <a href="https://github.com/bpham30">
+              <i className="fa fa-github-square"></i>
+            </a>
+            <a href="www.linkedin.com/in/bpham15">
+              <i className="fa fa-linkedin-square"></i>
+            </a>
+            <a href="tel:7707570294">
+              <i className="fa fa-phone-square"></i>
+            </a>
+            <a href="mailto:bangapham16@gmail.com">
+              <i className="fa fa-envelope-square"></i>
+            </a>
+
+              </div>
+          </div>
+  )
+}
--- src/components/Home/Profile/Profile.js
+++ src/components/Home/Profile/Profile.js
@@ -6,70 +6,67 @@ export default function Profile() {
   return (
     <div className="profile-container">
       <div className="profile-parent">
+        <div className="profile-pic-wrap">
+            <div className="profile-picture">
+        <div className="profile-picture-background"></div>
+      </div>
+        </div>
         <div className="profile-details">
-          <div className="colz">
+          {/* <div className="colz">
               <div className='colz-icon'>
             <a href="https://github.com/bpham30">
               <i className="fa fa-github-square"></i>
             </a>
             <a href="www.linkedin.com/in/bpham15">
-              <i class="fa fa-linkedin-square"></i>
+              <i className="fa fa-linkedin-square"></i>
             </a>
             <a href="tel:7707570294">
-              <i class="fa fa-phone-square"></i>
+              <i className="fa fa-phone-square"></i>
             </a>
             <a href="mailto:bangapham16@gmail.com">
-              <i class="fa fa-envelope-square"></i>
+              <i className="fa fa-envelope-square"></i>
             </a>
 
               </div>
-          </div>
+          </div> */}
 
           <div className="profile-details-name">
-              <span className="primary-text">
-                  {" "}
-                  Hello, I'm <span className="highlighted-text">Bang</span>
-              </span>
+            <span className="primary-text">
+              {" "}
+              Hello, I'm <span className="highlighted-text">Bang!</span>
+            </span>
           </div>
-          <div className='profile-details-role'>
-              <span className='primary-text'>
-                  {" "}
-                  <h1>
-                    {" "}
-                    <Typical
-                    loop={Infinity}
-                    steps={[
-                        "Undergraduate CS Major 🎓",
-                        1000,
-                        "Web Developer 💻",
-                        1000,
-                        "Graphic Designer 🎨",
-                        1000,
-                        "Amazon FBA Seller 📦",
-                        1000,
-                    ]}
-                    />
-                  </h1>
-                  <span className="profile-role-tagline">
-                  Motivated junior web developer seeking for a role as full-stack developer. 
-                  </span>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {" "}
+              <h1>
+                {" "}
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Comp Sci Major",
+                    1000,
+                    "Web Developer",
+                    1000,
+                    "Graphic Designer",
+                    1000,
+                    "Amazon Seller",
+                    1000,
+                  ]}
+                />
+              </h1>
+              <span className="profile-role-tagline">
+                based in Atlanta, GA.
               </span>
+            </span>
           </div>
+        </div>
           <div className="profile-options">
-              <button className='btn primary-btn'>
-                  {" "}
-                  Hire Me {" "}
-              </button>
-              <a href="https://docs.google.com/document/d/1YGUCKekvJDnlv5bO-MQFmHsnYs8QNeUi/edit?usp=sharing&ouid=103085667741888858416&rtpof=true&sd=true">
-                  <button className="btn highlighted-btn">Get Resume</button>
-              </a>
+            <button className="btn primary-btn"> Hire Me </button>
+            <a href="https://docs.google.com/document/d/1YGUCKekvJDnlv5bO-MQFmHsnYs8QNeUi/edit?usp=sharing&ouid=103085667741888858416&rtpof=true&sd=true">
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
           </div>
-        </div>
-        <div className='profile-picture'>
-            <div className='profile-picture-background'>
-
-            </div>
-        </div>
       </div>
     </div>
   );
