1bd37cf62b6a6a760a4e5fe22fd3f9ee6a9f0362
Bang Pham <bangpham@Bangs-Air.attlocal.net>
2022-02-05 06:46:38 -0500

    new navbar, landing page layout change, updated for mobile

--- src/components/Home/Navbar/Navbar.js
+++ src/components/Home/Navbar/Navbar.js
@@ -7,48 +7,36 @@ const Navbar = () =>{
     letter.current.scrollIntoView({ behavior: "smooth", block: "center" });
 
   return (
-    <div className="navbar-container">
-      <div className="navbar-parent">
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <img className="navbar-brand" href="#profileDiv" src="./logo.png"/> 
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item active">
-              <a className="nav-link" href="#profileDiv">
-                  ABOUT ME
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#homeDiv">
-                  PROJECTS
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="https://docs.google.com/document/d/1YGUCKekvJDnlv5bO-MQFmHsnYs8QNeUi/edit?usp=sharing&ouid=103085667741888858416&rtpof=true&sd=true">
-                  RESUME
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  CONTACT
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
+    <nav class="navbar navbar-dark bg-transparent fixed-top">
+    <div class="container-fluid">
+    <a class="navbar-brand" href="#">BANG PHAM</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">BANG PHAM</h5>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">ABOUT ME</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">PROJECTS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">RESUME</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">CONTACT</a>
+          </li>
+        </ul>
       </div>
     </div>
+  </div>
+</nav>
   );
 };
 
