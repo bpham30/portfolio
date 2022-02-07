74e2b15a24762bf425851eb497c5e665794711c6
Bang Pham <bangpham@Bangs-Air.attlocal.net>
2022-02-07 03:32:08 -0500

    deploy

--- src/components/Home/Profile/Profile.js
+++ src/components/Home/Profile/Profile.js
@@ -8,27 +8,9 @@ export default function Profile() {
       <div className="profile-parent">
         <div className="profile-pic-wrap">
             <div className="profile-picture">
-        <div className="profile-picture-background"></div>
-      </div>
         </div>
         <div className="profile-details">
-          {/* <div className="colz">
-              <div className='colz-icon'>
-            <a href="https://github.com/bpham30">
-              <i className="fa fa-github-square"></i>
-            </a>
-            <a href="www.linkedin.com/in/bpham15">
-              <i className="fa fa-linkedin-square"></i>
-            </a>
-            <a href="tel:7707570294">
-              <i className="fa fa-phone-square"></i>
-            </a>
-            <a href="mailto:bangapham16@gmail.com">
-              <i className="fa fa-envelope-square"></i>
-            </a>
 
-              </div>
-          </div> */}
 
           <div className="profile-details-name">
             <span className="primary-text">
@@ -68,6 +50,7 @@ export default function Profile() {
             </a>
           </div>
       </div>
+    </div>
     </div>
   );
 }
