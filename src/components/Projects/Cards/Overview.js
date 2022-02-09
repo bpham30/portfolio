8f3ebde647a68b6a7fd963be6fc2458d11098374
Bang Pham <bangpham@Bangs-Air.attlocal.net>
2022-02-09 03:23:00 -0500

    created card component for project section

--- src/components/Projects/Cards/Overview.js
+++ src/components/Projects/Cards/Overview.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { motion } from 'framer-motion';
+import { CardContext } from './context';
 import './Overview.css'
 
-export default function Overview() {
+export default function Overview(props) {
+
+    const { active, setActive } = useContext(CardContext);
+    const transition = {
+        type: "tween",
+    }
+
+    const switchToOpen = () => {
+        setActive("open");
+    }
+
+    let animate = {};
+    if (active === "overview")
+        animate = { x: 0};
+    else if(active === "open")
+        animate = {x: -290};
   return (
-    <div className='overview-container'>
+    <motion.div id='overview-bg' className='overview-container' animate={animate} transition={transition} >
         <div className="title-container">
-            <h1 className='title'>to-do</h1>
-            <h1 className='title'>list</h1>
-            <button className='btn card-btn'>Overview</button>
+            <h1 className='title'>{props.title}</h1>
+            <button onClick={switchToOpen}className='btn card-btn'>Overview</button>
         </div>
-    </div>
+    </motion.div>
   );
 }
