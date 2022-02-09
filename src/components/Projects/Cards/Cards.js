a8bf5ac5bca87a178ee9e091b6a525a663e6566d
Bang Pham <bangpham@Bangs-Air.attlocal.net>
2022-02-08 22:23:07 -0500

    making project cards for coding projects

--- src/components/Projects/Cards/Cards.js
+++ src/components/Projects/Cards/Cards.js
@@ -1,13 +1,15 @@
 import { eventWrapper } from '@testing-library/user-event/dist/utils';
 import React from 'react';
+import './Cards.css'
+import OpenCard from './OpenCard';
+import Overview from './Overview';
 
 export default function Cards() {
   return (
     <div className='card-container'>
-        <div className="card-parent">
-            
-            <div className="card-wrapper"></div>
-        </div>
+            <div className="card-wrapper">
+              <OpenCard/>
+            </div>
     </div>
   )
 }
